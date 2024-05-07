64c42be7c5ee39d65be3d3ba9123331b828bddb6
unknown <pravinkpr007@gmail.com>
2024-05-07 19:30:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const handleClick = (event) => {
+    setPosition({
+      x: event.clientX,
+      y: event.clientY,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="myApp" style={{ width: "100vw", height: "100vh" }} onClick={handleClick}>
+      <div
+        style={{
+          position: "absolute",
+          width: "50px",
+          height: "50px",
+          backgroundColor: "red",
+          left: position.x + "px",
+          top: position.y + "px",
+          transitionDuration: "3s",
+        }}
+      />
     </div>
   );
-}
+};
 
 export default App;
